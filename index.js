f996335d3928211d813d1dbcbf8563f5145b7293
Dens97 <lessing.dennis@gmail.com>
2025-05-02 22:21:59 +0200

    Add Checkbox inputs to customize password generation

--- index.js
+++ index.js
@@ -1,30 +1,4 @@
-const characters = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
+const alphabetSmallArr = [
   "a",
   "b",
   "c",
@@ -51,16 +25,39 @@ const characters = [
   "x",
   "y",
   "z",
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
+];
+
+const alphabetLargeArr = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const symbols = [
   "~",
   "`",
   "!",
@@ -99,22 +96,57 @@ const inputError = document.getElementById("input-error");
 
 const firstPasswordEl = document.getElementById("firstPassword");
 const secondPasswordEl = document.getElementById("secondPassword");
+const checkboxError = document.getElementById("checkbox-error");
+
+const alphabetSmall = document.getElementById("alphabet-small");
+const alphabetLarge = document.getElementById("alphabet-large");
+const numbersEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("symbols");
 
 function generatePasswords() {
+  if (
+    !alphabetSmall.checked &&
+    !alphabetLarge.checked &&
+    !numbersEl.checked &&
+    !symbolsEl.checked
+  ) {
+    console.log("All unchecked");
+    checkboxError.classList.remove("hidden");
+    return;
+  }
+
+  checkboxError.classList.add("hidden");
+
   //getrandompassword and display it x2
+
+  let customArray = [];
+  if (alphabetSmall.checked) {
+    customArray = customArray.concat(alphabetSmallArr);
+    console.log(customArray);
+  }
+  if (alphabetLarge.checked) {
+    customArray = customArray.concat(alphabetLargeArr);
+  }
+  if (numbersEl.checked) {
+    customArray = customArray.concat(numbers);
+  }
+  if (symbolsEl.checked) {
+    customArray = customArray.concat(symbols);
+  }
+
   if (inputEl.value > 0 && inputEl.value <= 20) {
     inputError.textContent = "";
-    firstPasswordEl.textContent = getPassword();
-    secondPasswordEl.textContent = getPassword();
+    firstPasswordEl.textContent = getPassword(customArray);
+    secondPasswordEl.textContent = getPassword(customArray);
   } else {
     inputError.textContent = "Please specify a number between 0-20!";
   }
 }
 
-function getPassword() {
+function getPassword(array) {
   let password = "";
   for (let i = 0; i < inputEl.value; i++) {
-    password += characters[Math.floor(Math.random() * characters.length)];
+    password += array[Math.floor(Math.random() * array.length)];
   }
   return password;
 }
